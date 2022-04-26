b08b4d8efa571c848cd38efd6b96bd7c03de40e8
ramanda ajisaka <tamvam.pemberani@gmail.com>
2022-04-26 14:57:52 +0700

    Upload image endpoint API

--- routes/media.js
+++ routes/media.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const isBase64 = require("is-base64");
+const base64Img = require("base64-img");
+const fs = require("fs");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const { Media } = require("../models");
+const { HOSTNAME } = process.env;
+
+router.post("/", (req, res) => {
+  const image = req.body.image;
+
+  if (!isBase64(image, { mimeRequired: true })) {
+    return res.status(400).json({ status: "error", message: "invalid base64" });
+  }
+
+  base64Img.img(image, "./public/images", Date.now(), async (err, filepath) => {
+    if (err) {
+      return res.status(400).json({ status: "error", message: err.message });
+    }
+
+    const filename = filepath.split("\\").pop().split("/").pop();
+
+    const media = await Media.create({ image: `images/${filename}` });
+
+    return res.json({
+      status: "success",
+      data: {
+        id: media.id,
+        image: `${HOSTNAME}/images/${filename}`,
+      },
+    });
+  });
 });
 
 module.exports = router;
